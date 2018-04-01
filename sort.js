7c7f08e5aa64bae798d9b503899082715ad1fc3c
Saheed Oladele <sdrfngg640@gmail.com>
2018-04-01 09:27:04 +0100

    Sort by length js

---
+++ sort.js
@@ -0,0 +1,11 @@
+const allStates = ["Abia", "Adamawa", "Anambra", "Akwa Ibom", "Bauchi", "Bayelsa", "Benue", "Borno", "Cross River", "Delta", "Ebonyi", "Enugu", "Edo", "Ekiti", "Gombe", "Imo", "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nasarawa", "Niger", "Ogun", "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara"];
+
+const sortByNameLength = (states) => {
+  return allStates.sort(function(a,b){
+    return b.length - a.length;
+  });
+}
+
+const sorted = sortByNameLength(allStates);
+console.log(sorted[19]);
+
